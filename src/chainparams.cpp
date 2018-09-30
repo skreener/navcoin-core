284f0356d4eefb0b1cb6173f9608e1cebc792804
v xela <alex@encrypt-s.com>
2018-09-30 02:08:41 +0200

    zerocoin consensus params

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -1,5 +1,6 @@
 // Copyright (c) 2010 Satoshi Nakamoto
 // Copyright (c) 2009-2015 The Bitcoin Core developers
+// Copyright (c) 2018 The NavCoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -67,6 +68,17 @@ static CBlock CreateGenesisBlockTestnet(uint32_t nTime, uint32_t nNonce, uint32_
     return CreateGenesisBlock(pszTimestamp, genesisOutputScript, nTime, nNonce, nBits, nVersion, genesisReward);
 }
 
+libzerocoin::ZerocoinParams* Consensus::Params::Zerocoin_Params() const
+{
+    assert(this);
+    static CBigNum bnDecModulus = 0;
+    if (!bnDecModulus)
+        bnDecModulus.SetDec(zerocoinModulus);
+    static libzerocoin::ZerocoinParams ZCParamsDec = libzerocoin::ZerocoinParams(bnDecModulus);
+
+    return &ZCParamsDec;
+}
+
 /**
  * Main network
  */
@@ -120,7 +132,15 @@ public:
         consensus.nCyclesPaymentRequestVoting = 4;
         consensus.nPaymentRequestMaxVersion = 2;
         consensus.nProposalMaxVersion = 2;
-	consensus.nMaxFutureDrift = 60;
+        consensus.nMaxFutureDrift = 60;
+
+        /** Zerocoin */
+        consensus.zerocoinModulus = "25195908475657893494027183240048398571429282126204032027777137836043662020707595556264018525880784"
+                                    "4069182906412495150821892985591491761845028084891200728449926873928072877767359714183472702618963750149718246911"
+                                    "6507761337985909570009733045974880842840179742910064245869181719511874612151517265463228221686998754918242243363"
+                                    "7259085141865462043576798423387184774447920739934236584823824281198163815010674810451660377306056201619676256133"
+                                    "8441436038339044149526344321901146575444541784240209246165157233507787077498171257724679629263863563732899121548"
+                                    "31438167899885040445364023527381951378636564391212010397122822120720357";
 
         consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].bit = 28;
         consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].nStartTime = 1199145601; // January 1, 2008
